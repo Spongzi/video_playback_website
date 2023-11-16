87a80fa1f48ddae60e4b162eb8706f4c934cf41a
spongzi <spongzicode@163.com>
2023-11-16 21:09:23 +0800

    feat：重写HttpMessageConverters

---
+++ video-playback-website-service/src/main/java/com/spongzi/config/JsonHttpMessageConvertConfig.java
@@ -0,0 +1,35 @@
+package com.spongzi.config;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
+import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class JsonHttpMessageConvertConfig {
+
+    @Bean
+    @Primary
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat,
+                SerializerFeature.WriteMapNullValue,
+                SerializerFeature.WriteNullStringAsEmpty,
+                SerializerFeature.WriteNullNumberAsZero,
+                SerializerFeature.WriteNullBooleanAsFalse,
+                SerializerFeature.WriteNullListAsEmpty,
+                SerializerFeature.DisableCircularReferenceDetect,
+                SerializerFeature.WriteDateUseDateFormat,
+                SerializerFeature.WriteEnumUsingToString,
+                SerializerFeature.WriteClassName
+        );
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        return new HttpMessageConverters(fastConverter);
+    }
+}
