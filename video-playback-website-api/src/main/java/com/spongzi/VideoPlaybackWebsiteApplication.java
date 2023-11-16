3df4581da795d2261ae0ee5d3a5c91a13c5ead25
spongzi <spongzicode@163.com>
2023-11-16 20:26:11 +0800

    init: 初始化SpringBoot项目

---
+++ video-playback-website-api/src/main/java/com/spongzi/VideoPlaybackWebsiteApplication.java
@@ -0,0 +1,17 @@
+package com.spongzi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 视频播放网站应用
+ *
+ * @author spong
+ * @date 2023/11/16
+ */
+@SpringBootApplication
+public class VideoPlaybackWebsiteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VideoPlaybackWebsiteApplication.class, args);
+    }
+}
